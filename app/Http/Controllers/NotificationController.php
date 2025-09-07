8b3c94e95964d41168d2d31824dfab1198f0d120
NgocNguyen1503 <feyd153@gmail.com>
2025-09-07 22:55:21 +0700

    update notifications' time

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Common;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseAPI;
 use App\Models\Push;
@@ -12,20 +13,27 @@ class NotificationController extends Controller
 {
     private $responseApi;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->responseApi = new ResponseApi();
     }
 
-    public function listNotification (Request $request) {
+    public function listNotification(Request $request)
+    {
         $param = $request->all();
         $pushes = Push::select(
-            'id', 'title', 'content', 'status', 'user_id', 'created_at'
+            'id',
+            'title',
+            'content',
+            'status',
+            'user_id',
+            'created_at'
         )
             ->where('user_id', Auth::user()->id)
             ->orderBy('created_at', 'DESC')
             ->skip($param['offset'])->take($param['limit'])
             ->get()->map(function ($noti) {
-                $noti->time_ago = Carbon::parse($noti->created_at)->diffForHumans();
+                $noti->time_ago = Common::timeAgo($noti->created_at);
                 return $noti;
             });
         return $this->responseApi->success($pushes);
